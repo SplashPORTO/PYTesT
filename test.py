b63228ebdb4042bcd8cf2a094a30f01f380f759a
Splash <splash@mail.pt>
2018-12-27 19:28:07 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+
+import urllib.request
+
+def set_password():
+    password='1234567890'
+
+set_password()
+
+
+print(set_password())
+
+
+def send_to_facebook():
+    msg = "Teste de publicação"
